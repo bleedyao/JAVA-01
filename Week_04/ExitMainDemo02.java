49bdd3ba0d892b690a642c0f250752a2c8d57ada
Luhao Yao <yaoluhao@sina.com>
2021-02-01 21:53:54 +0800

    update the demo 02

---
+++ Week_04/ExitMainDemo02.java
@@ -0,0 +1,13 @@
+import task.AsyncRunnable02;
+
+public class ExitMainDemo02 {
+    public static void main(String[] args) throws InterruptedException {
+        AsyncRunnable02 ar = new AsyncRunnable02(result ->
+                System.out.println(ExitMainDemo01.class.getName() + " result is " + result)
+        );
+        Thread thread = new Thread(ar);
+        thread.start();
+        thread.join();
+        System.out.println("Main Thread is exit");
+    }
+}
