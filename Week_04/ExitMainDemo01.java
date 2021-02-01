68eaa5b7933156311779917ef3fe99863b4e088d
Luhao Yao <Luhao.Yao@partner.bmwgroup.com>
2021-02-01 17:44:46 +0800

    update the demo 01 readme

--- Week_04/ExitMainDemo01.java
+++ Week_04/ExitMainDemo01.java
@@ -1,8 +1,8 @@
-import task.AsyncRunnable;
+import task.AsyncRunnable01;
 
 public class ExitMainDemo01 {
     public static void main(String[] args) throws InterruptedException {
-        AsyncRunnable ar = new AsyncRunnable(result ->
+        AsyncRunnable01 ar = new AsyncRunnable01(result ->
                 System.out.println(ExitMainDemo01.class.getName() + " result is " + result)
         );
         new Thread(ar).start();
