dfc20e84f3157b238fddb4c0b1fb98ef6dc82d98
Luhao Yao <Luhao.Yao@partner.bmwgroup.com>
2021-02-01 16:51:10 +0800

    finish the exit main demo 01

--- Week_04/ExitMainDemo01.java
+++ Week_04/ExitMainDemo01.java
@@ -1,11 +1,14 @@
 import task.AsyncRunnable;
 
 public class ExitMainDemo01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         AsyncRunnable ar = new AsyncRunnable(result ->
                 System.out.println(ExitMainDemo01.class.getName() + " result is " + result)
         );
         new Thread(ar).start();
+        synchronized (ar) {
+            ar.wait();
+        }
         System.out.println("Main Thread is exit");
     }
 }
--- Week_04/task/AsyncRunnable.java
+++ Week_04/task/AsyncRunnable.java
@@ -1,19 +1,24 @@
 package task;
 
-public class AsyncRunnable implements Runnable{
+public class AsyncRunnable implements Runnable {
     private Listener listener = null;
+
     public AsyncRunnable(Listener listener) {
         this.listener = listener;
     }
+
     @Override
     public void run() {
         if (listener != null) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             listener.onListener(1);
+            synchronized (this) {
+                this.notify();
+            }
         }
     }
 }
