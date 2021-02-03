f4a6f5477ac0bc57939ebe5a0d2860b403a8044f
Luhao Yao <Luhao.Yao@partner.bmwgroup.com>
2021-02-03 15:09:43 +0800

    update the atmicInteger in the main thread

--- Week_04/ExitMainDemo02.java
+++ Week_04/ExitMainDemo02.java
@@ -1,13 +1,18 @@
 import task.AsyncRunnable02;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ExitMainDemo02 {
     public static void main(String[] args) throws InterruptedException {
-        AsyncRunnable02 ar = new AsyncRunnable02(result ->
-                System.out.println(ExitMainDemo01.class.getName() + " result is " + result)
-        );
+        AtomicInteger mainResult = new AtomicInteger(Integer.MIN_VALUE);
+        AsyncRunnable02 ar = new AsyncRunnable02(result -> {
+            mainResult.set(result);
+            System.out.println(ExitMainDemo01.class.getName() + " result is " + result);
+        });
         Thread thread = new Thread(ar);
         thread.start();
         thread.join();
+        System.out.println("main result: "+ mainResult);
         System.out.println("Main Thread is exited");
     }
 }
--- Week_04/ExitMainDemo03.java
+++ Week_04/ExitMainDemo03.java
@@ -1,15 +1,18 @@
 import task.AsyncRunnable02;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ExitMainDemo03 {
     public static void main(String[] args) throws InterruptedException {
+        AtomicInteger mainResult = new AtomicInteger(Integer.MIN_VALUE);
         Lock lock = new ReentrantLock(true);
         Condition condition = lock.newCondition();
         AsyncRunnable02 ar = new AsyncRunnable02(result -> {
             lock.lock();
+            mainResult.set(result);
             System.out.println(ExitMainDemo01.class.getName() + " result is " + result);
             condition.signal();
             lock.unlock();
@@ -18,6 +21,7 @@ public class ExitMainDemo03 {
         thread.start();
         lock.lock();
         condition.await();
+        System.out.println("main result: " + mainResult);
         System.out.println("Main Thread is exited");
         lock.unlock();
     }
